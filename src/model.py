f47b071a7de555a4dc613acc0755ac7d885b0597
kvxb <justin.raindust@gmail.com>
2026-09-07 05:00:28 +0300

    (core): add full model architecture code
    idk what to say specifically, this time i wrote it myself hopefully i
    understand more of it

--- src/model.py
+++ src/model.py
@@ -3,6 +3,9 @@ import torch
 import torch.nn as nn
 
 class PositionalEncoding(nn.Module):
+    # Type annotation for static analyzers
+    sin_pos_enc: torch.Tensor     
+
     def __init__(self, d_model, max_len=41):
         super().__init__()
         positional_encodings = torch.zeros(max_len, d_model)
@@ -43,4 +46,39 @@ class HoldEmbedding(nn.Module):
         r_emb = self.r_lk_t(x[:, :, 2].long())
 
         return x_emb + y_emb + r_emb
+
+class Model(nn.Module):
+    def __init__(self, vocab_x, vocab_y, vocab_r, d_model = 128, nhead = 8, num_layers = 4):
+        super().__init__()
+        self.embedding = HoldEmbedding(d_model, vocab_x, vocab_y, vocab_r)
+        self.meta_proj = nn.Linear(2, d_model)
+        self.pos_encoder = PositionalEncoding(d_model)
+
+        layer = nn.TransformerDecoderLayer(d_model, nhead, 512, batch_first=True)
+        self.decoder = nn.TransformerDecoder(layer, num_layers)
+
+        #TODO recap how these work
+        #How can you reproject the output of the decoder on these?
+        self.x_ll = nn.Linear(d_model, vocab_x)
+        self.y_ll = nn.Linear(d_model, vocab_y)
+        self.r_ll = nn.Linear(d_model, vocab_r)
+
+    def forward(self, meta, holds):
+        padding_mask = (holds[:, :, 2] == 0)
+        meta_mask = torch.zeros(padding_mask.size(0), 1, dtype=torch.bool, device=padding_mask.device)
+        padding_mask = torch.cat([meta_mask, padding_mask], dim=1)
+
+        hold_emb = self.embedding(holds)
+        meta_emb = self.meta_proj(meta).unsqueeze(1)
+
+        complete_tensor = torch.cat([meta_emb, hold_emb], dim=1)
+
+        complete_tensor = self.pos_encoder(complete_tensor)
+
+        causal_mask = nn.Transformer.generate_square_subsequent_mask(complete_tensor.size(1), device=complete_tensor.device)
+
+        out = self.decoder(complete_tensor, complete_tensor, tgt_mask=causal_mask, tgt_key_padding_mask=padding_mask)
+
+        return self.x_ll(out), self.y_ll(out), self.r_ll(out)
+
 
